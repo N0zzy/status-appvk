03a76ee3ba81267ac8364da6f2d0cd28bba2f4bc
N0zzy <ffnn83@yandex.ru>
2019-01-10 08:10:29 +0500

    update #0002

---
+++ src/App/VK.php
@@ -0,0 +1,28 @@
+<?php
+namespace Status\App;
+
+/**
+ * Class VK
+ * @package Status\App
+ */
+class VK
+{
+    /**
+     * @return VK
+     */
+    public static function auth($response)
+    {
+
+        return new self();
+    }
+
+    public function echo()
+    {
+
+    }
+
+    public function return()
+    {
+
+    }
+}
