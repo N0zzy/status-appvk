ee9f9fedbad9dde657e131c919f2c096fc1b7148
N0zzy <ffnn83@yandex.ru>
2019-01-10 09:18:18 +0500

    update #0003

--- src/App/VK.php
+++ src/App/VK.php
@@ -7,22 +7,89 @@ namespace Status\App;
  */
 class VK
 {
-    /**
-     * @return VK
-     */
-    public static function auth($response)
+    private static $response;
+
+    public static function get($response)
     {
 
         return new self();
     }
 
-    public function echo()
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function sid()
     {
-
+        return self::getValueResponse('sid');
     }
 
-    public function return()
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function aid()
     {
+        return self::getValueResponse('api_id');
+    }
 
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function uid()
+    {
+        return self::getValueResponse('user_id');
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function vid()
+    {
+        return self::getValueResponse('viewer_id');
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function token()
+    {
+        return self::getValueResponse('access_token');
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function secret()
+    {
+        return self::getValueResponse('secret');
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function authKey()
+    {
+        return self::getValueResponse('auth_key');
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws \Exception
+     */
+    private static function getValueResponse(string $key)
+    {
+        if(!array_key_exists($key, self::$response))
+        {
+            throw new \Exception('error response app key');
+        }
+
+        return self::$response[$key];
     }
 }
